7af35e075dd0c741235d28e05bb597de66a935dc
aloisdg <aloisdegouvello@live.fr>
2015-04-08 20:10:05 +0200

    Add doc and degree

--- edX/Module1/Program.cs
+++ edX/Module1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,6 +9,9 @@ namespace Module1
 {
 	class Program
 	{
+		/// <summary>
+		/// Facebook allow 71 gender options
+		/// </summary>
 		enum Gender
 		{
 			Male,
@@ -22,10 +26,22 @@ namespace Module1
 			Other
 		}
 
+		enum DegreeType
+		{
+			Bachelor,
+			Master,
+			PhD,
+			Other
+		}
+
+		/// <summary>
+		/// AddressLine3 is required for some countries like France
+		/// </summary>
 		class Address
 		{
 			public string AddressLine1 { get; set; }
 			public string AddressLine2 { get; set; }
+			public string AddressLine3 { get; set; }
 			public string City { get; set; }
 			public string State { get; set; }
 			public string Zip { get; set; }
@@ -51,6 +67,21 @@ namespace Module1
 		class Professor : APerson
 		{
 			public override SchoolStatus SchoolStatus { get { return SchoolStatus.Professor; }}
+		}
+
+
+		// TODO : Idea for a degreeType notation ? I dont want to keep hungarian
+		interface IDegree
+		{
+			string Name { get; set; }
+			//string DegreeType Dt { get; set; }
+		}
+
+		class University
+		{
+			IEnumerable<Student> Students { get; set; }
+			IEnumerable<Professor> Professors { get; set; }
+			private IEnumerable<IDegree> Degrees { get; set; }
 		}
 
 		static void Main(string[] args)
