4d35e6678c44b524c66d88e49c705eec5496ed73
aloisdg <aloisdegouvello@live.fr>
2015-04-08 19:04:20 +0200

    Add person support

--- edX/Module1/Program.cs
+++ edX/Module1/Program.cs
@@ -8,6 +8,51 @@ namespace Module1
 {
 	class Program
 	{
+		enum Gender
+		{
+			Male,
+			Female,
+			Other
+		}
+
+		enum SchoolStatus
+		{
+			Student,
+			Professor,
+			Other
+		}
+
+		class Address
+		{
+			public string AddressLine1 { get; set; }
+			public string AddressLine2 { get; set; }
+			public string City { get; set; }
+			public string State { get; set; }
+			public string Zip { get; set; }
+			public string Country { get; set; }
+		}
+
+		abstract class APerson
+		{
+			public string FirstName { get; set; }
+			public string LastName { get; set; }
+			public DateTime Birthdate { get; set; }
+			public Address Address { get; set; }
+			public Gender Gender { get; set; }
+
+			public abstract SchoolStatus SchoolStatus { get; }
+		}
+
+		class Student : APerson
+		{
+			public override SchoolStatus SchoolStatus { get { return SchoolStatus.Student; }}
+		}
+
+		class Professor : APerson
+		{
+			public override SchoolStatus SchoolStatus { get { return SchoolStatus.Professor; }}
+		}
+
 		static void Main(string[] args)
 		{
 		}
