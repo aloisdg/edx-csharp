ebec9cf4297027a4cef76d3760eacb22489b1027
aloisdg <aloisdegouvello@live.fr>
2015-04-08 18:32:38 +0200

    Add module 2

---
+++ edX/Module2/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Module2
+{
+	class Program
+	{
+		static void Main()
+		{
+			Print1();
+			Console.ReadLine();
+		}
+
+		static void Print1()
+		{
+			for (var i = 0; i < 8; i++)
+				for (var j = 0; j < 4; j++)
+					Console.Write((i % 2 == 0 ? "XO" : "OX")
+						+ (j == 3 ? "\n" : ""));
+		}
+
+		static void Print2()
+		{
+			for (var i = 0; i < 8; i++)
+				for (var j = 0; j < 1; j++) // you ask fo a nested loop ...
+					Console.WriteLine(i % 2 == 0 ? "XOXOXOXO" : "OXOXOXOX");
+		}
+
+		static void Print3()
+		{
+			for (; false; ) for (; ; ) if (true) Console.WriteLine("you ask fo a nested loop");
+
+			Console.Write("XOXOXOXO\n"
+					+ "OXOXOXOX\n"
+					+ "XOXOXOXO\n"
+					+ "OXOXOXOX\n"
+					+ "XOXOXOXO\n"
+					+ "OXOXOXOX\n"
+					+ "XOXOXOXO\n"
+					+ "OXOXOXOX");
+		}
+	}
+}
