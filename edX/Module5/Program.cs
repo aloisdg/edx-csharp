198fd3b3bed0f7a71479a49042b0f06b9029e973
alois <aloisdegouvello@live.fr>
2015-04-20 23:07:42 +0200

    Add complete Module5

--- edX/Module5/Program.cs
+++ edX/Module5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,9 +9,148 @@ namespace Module5
 {
 	class Program
 	{
-		static void Main(string[] args)
+		public enum SchoolStatus
 		{
+			Student,
+			Teacher,
+			Other
+		}
 
+		public class UProgram
+		{
+			private string Name { get; set; }
+			public Degree Degree { get; set; }
+
+			public UProgram(string name)
+			{
+				Name = name;
+			}
+
+			public override string ToString()
+			{
+				return "The " + Name
+				       + " program contains the "
+				       + Degree.Name + " degree";
+			}
+		}
+
+		public class Degree
+		{
+			public string Name { get; private set; }
+			public Course Course { get; set; }
+
+			public Degree(string name)
+			{
+				Name = name;
+			}
+
+			public override string ToString()
+			{
+				return "The " + Name
+				       + " degree contains the course "
+				       + Course.Name;
+			}
+		}
+
+		public class Course
+		{
+			public string Name { get; private set; }
+			public IEnumerable<Student> Students { get; set; }
+
+			private IEnumerable<Teacher> _teachers;
+			public IEnumerable<Teacher> Teachers
+			{
+				get { return _teachers; }
+				set
+				{
+					// Instantiate at least one Teacher object.
+					if (!value.Any()) 
+						throw new Exception("Instantiate at least one Teacher object.");
+					_teachers = value;
+				}
+			}
+
+			public Course(string name)
+			{
+				Name = name;
+			}
+
+			public override string ToString()
+			{
+				return "The " + Name
+				       + " course contains " + Students.Count()
+				       + " student<s>";
+			}
+		}
+
+		public abstract class APerson
+		{
+			public string FirstName { get; set; }
+			public string LastName { get; set; }
+			public DateTime Birthdate { get; set; }
+
+			public abstract SchoolStatus SchoolStatus { get; }
+
+			protected APerson(string firstName, string lastName, DateTime birthdate)
+			{
+				FirstName = firstName;
+				LastName = lastName;
+				Birthdate = birthdate;	
+			}
+		}
+
+		public class Student : APerson
+		{
+			public override SchoolStatus SchoolStatus { get { return SchoolStatus.Student; } }
+
+			public Student(string firstName, string lastName, DateTime birthdate)
+				: base(firstName, lastName, birthdate) {}
+		}
+
+		public class Teacher : APerson
+		{
+			public override SchoolStatus SchoolStatus { get { return SchoolStatus.Teacher; } }
+
+			public Teacher(string firstName, string lastName, DateTime birthdate)
+				: base(firstName, lastName, birthdate) {}
+		}
+
+		private static void Main()
+		{
+			// Instantiate three Student objects.
+			var harry = new Student("Harry", "Potter", new DateTime(1980, 7, 31));
+			var ron = new Student("Ron", "Weasley", new DateTime(1980, 3, 1));
+			var hermione = new Student("Hermione", "Granger", new DateTime(1979, 9, 19));
+
+			// Instantiate a Course object called Programming with C#.
+			var course = new Course("Programming with C#")
+			{
+				// Add your three students to this Course object.
+				Students = new Collection<Student> { harry, ron, hermione },
+				// Add that Teacher object to your Course object.
+				Teachers = new Collection<Teacher> { new Teacher("Remus",  "Lupin", new DateTime(1960, 3, 10)) }
+			};
+
+			// Instantiate a Degree object, such as Bachelor.
+			var degree = new Degree("Bachelor")
+			{
+				// Add your Course object to the Degree object.
+				Course = course
+			};
+
+			// Instantiate a UProgram object called Information Technology.
+			var uProgram = new UProgram("Information Technology")
+			{
+				// Add the Degree object to the UProgram object.
+				Degree = degree
+			};
+
+			// Using Console.WriteLine statements, output the following information to the console window:
+			Console.WriteLine(uProgram + Environment.NewLine);
+			Console.WriteLine(degree + Environment.NewLine);
+			Console.WriteLine(course.ToString());
+
+			Console.ReadKey();
 		}
 	}
 }
