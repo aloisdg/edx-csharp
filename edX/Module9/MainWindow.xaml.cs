06ced0d9e4d425e07e0fe2113128a0bd15d9d33a
aloisdg <aloisdegouvello@live.fr>
2015-05-04 12:26:19 +0200

    Add the first five answers

--- edX/Module9/MainWindow.xaml.cs
+++ edX/Module9/MainWindow.xaml.cs
@@ -16,14 +16,58 @@ using System.Windows.Shapes;
 //  TODO : Move to "Mod_9_Homework"
 namespace Module9
 {
-	/// <summary>
-	/// Interaction logic for MainWindow.xaml
-	/// </summary>
 	public partial class MainWindow : Window
 	{
+		// Add a new class to the project to represent a Student with three properties for the text fields.  
+		public class Student
+		{
+			public string FirstName { get; private set; }
+			public string LastName { get; private set; }
+			public string Program { get; private set; }
+
+			public Student(string firstName, string lastName, string program)
+			{
+				FirstName = firstName;
+				LastName = lastName;
+				Program = program;
+			}
+		}
+
+		// Create a collection to store Student objects.
+		private List<Student> _students = new List<Student>();
+
 		public MainWindow()
 		{
 			InitializeComponent();
+
+			// Using the form and button, create a number of Student objects and add them to the collection (at least 3)
+			// Loaded += MainWindow_Loaded;
+		}
+
+		void MainWindow_Loaded(object sender, RoutedEventArgs e)
+		{
+			const int studentCount = 3;
+			for (var i = 0; i < studentCount; i++)
+			{
+				txtFirstName.Text = "Harry" + i;
+				txtLastName.Text = "Potter" + i;
+				txtCity.Text = "Little Whinging" + i;
+				btnCreateStudent_Click(this, new RoutedEventArgs());
+			}
+		}
+
+		// Implement the code in the button click event handler to create a Student object and add it to the collection
+		private void btnCreateStudent_Click(object sender, RoutedEventArgs e)
+		{
+			var firstName = !String.IsNullOrEmpty(txtFirstName.Text) ? txtFirstName.Text : String.Empty;
+			var lastName = !String.IsNullOrEmpty(txtLastName.Text) ? txtLastName.Text : String.Empty;
+			var program = !String.IsNullOrEmpty(txtCity.Text) ? txtCity.Text : String.Empty; // they made a typo here?
+			_students.Add(new Student(firstName, lastName, program));
+
+			// Clear the contents of the text boxes in the event handler.
+			txtFirstName.Clear();
+			txtLastName.Clear();
+			txtCity.Clear();
 		}
 	}
 }
