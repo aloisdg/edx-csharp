2c7944906275384b324bdb9e46be98388a1e42da
aloisdg <aloisdegouvello@live.fr>
2015-05-04 19:20:03 +0200

    Add solutions 3,4,30,32

--- edX/ModuleA/Program.cs
+++ edX/ModuleA/Program.cs
@@ -10,6 +10,34 @@ namespace ModuleA
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("Hello World");
 		}
+
+		// Linq3() // 71 results
+		// This sample uses the where clause to find all products that are in stock and 
+		// cost more than 3.00 per unit.
+		
+		// var expensiveInStockProducts = products.Where(p => p.UnitsInStock > 0 && p.UnitPrice > (decimal)3.00);
+
+		// Linq4() // 3 results 
+		// This sample uses the where clause to find all customers in Washington.
+		
+		// var waCustomers = from c in customers
+		//		let isNotNull = !String.IsNullOrWhiteSpace(c.Region)
+		//		where isNotNull && c.Region.Equals("WA")
+		//		select c;
+
+		// Linq30()
+		// This sample uses orderby to sort a list of products by name.
+		// Use the \"descending\" keyword at the end of the clause to perform a reverse ordering."
+		
+		// var sortedProducts = from product in products
+		//		orderby product.ProductName descending
+		//		select product;
+
+		// Linq32()
+		// "This sample uses orderby and descending to sort a list of doubles from highest to lowest."
+		
+		// var sortedDoubles = doubles.OrderByDescending(d => d);
 	}
 }
