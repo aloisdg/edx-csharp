077d9f57915ff8d7ddaa1b125f37bfaff898f803
aloisdg <aloisdegouvello@live.fr>
2015-05-14 15:04:22 +0200

    Update ModuleB

---
+++ edX/ModuleB/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModuleB
+{
+	class Student
+	{
+		private string firstName;
+		private string lastName;
+		private string city;
+
+		public string FirstName
+		{
+			get { return firstName; }
+			set { firstName = value; }
+		}
+
+
+		public string LastName
+		{
+			get { return lastName; }
+			set { lastName = value; }
+		}
+
+
+		public string City
+		{
+			get { return city; }
+			set { city = value; }
+		}
+	}
+}
